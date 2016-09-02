33a4114f9522c0e5675dce66b8d435fd7d6cc9cc
mataprasad045 <mataprasad045@gmail.com>
2016-09-01 18:19:57 -0700

    final

---
+++ Kx.KxCheckBoxCell.Demo/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kx.KxCheckBoxCell.Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add(new KxCheckBoxColumn(dataGridView1) { Name = "SELECT" });
+            var list = new List<AAA>();
+            list.Add(new AAA() { X = "1", Y = "1" });
+            list.Add(new AAA() { X = "2", Y = "2" });
+            list.Add(new AAA() { X = "3", Y = "3" });
+            dataGridView1.DataSource = list;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+                label1.Text += "\n" + ((KxCheckBoxCell)item.Cells[0]).Checked;
+            }
+        }
+    }
+
+    public class AAA
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+}
---
+++ Kx.KxCheckBoxCell/KxCheckBoxCell.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using Kx.KxCheckBoxCell.Properties;
+
+namespace Kx.KxCheckBoxCell
+{
+    public class KxCheckBoxCell : System.Windows.Forms.DataGridViewImageCell
+    {
+        public bool Checked { get; set; }
+    }
+
+    public class KxCheckBoxColumn : System.Windows.Forms.DataGridViewImageColumn
+    {
+        private DataGridView grid = null;
+
+        public KxCheckBoxColumn(DataGridView grid)
+        {
+            Resources.x_on.Tag = true;
+            Resources.x_off.Tag = false;
+
+            this.Image = Resources.x_off;
+            this.CellTemplate = new KxCheckBoxCell();
+
+            this.grid = grid;
+            this.grid.CellClick += new DataGridViewCellEventHandler(DataGridView_CellClick);
+        }
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1 && this.grid.Columns[e.ColumnIndex].Name == this.Name)
+            {
+                var cell = ((KxCheckBoxCell)this.grid.Rows[e.RowIndex].Cells[this.Name]);
+                if (cell != null)
+                {
+                    if (cell.Checked)
+                    {
+                        cell.Value = Resources.x_off;
+                        cell.Checked = false;
+                    }
+                    else
+                    {
+                        cell.Value = Resources.x_on;
+                        cell.Checked = true;
+                    }
+                }
+            }
+        }
+    }
+}
